2fdea8e18b1ca727f9ee77e989f66daecc6be440
LzGuimaraes <luizsantosleventi19901@gmail.com>
2025-09-26 16:37:01 -0400

    Fix: Ajustando a coluna email para unique

--- src/main/java/dev/Cursos/cursos/User/UserModel.java
+++ src/main/java/dev/Cursos/cursos/User/UserModel.java
@@ -1,25 +1,39 @@
 package dev.Cursos.cursos.User;
 
+import dev.Cursos.cursos.Curso.CursoModel;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tb_user")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Table(name = "tb_user")
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_user;
     private String nome;
     private int idade;
+    @Column(unique = true)
     private String email;
     private String password;
+
+    @ManyToMany
+    @JoinColumn(name = "id_curso")
+    private CursoModel curso;
+
+    @ManyToOne
+    @JoinColumn(name = "id_post")
+    private Long posts;
 }
