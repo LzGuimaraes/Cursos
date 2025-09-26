1d4a7ded27d395b2d3e5495d4534a87817772007
LzGuimaraes <luizsantosleventi19901@gmail.com>
2025-09-25 22:04:12 -0400

    first commit

---
+++ src/main/java/dev/Cursos/cursos/CursosApplication.java
@@ -0,0 +1,14 @@
+package dev.Cursos.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosApplication.class, args);
+		System.out.println("Aplicação iniciada com sucesso!");
+	}
+
+}
