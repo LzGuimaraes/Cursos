80662b6b885b38ad0edcbebf1e38e93ca8281803
LzGuimaraes <luizsantosleventi19901@gmail.com>
2025-10-02 22:13:14 -0400

    Feat: Adiciona Security com configuração STATELESS

---
+++ src/main/java/dev/Cursos/cursos/Security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package dev.Cursos.cursos.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable()).
+        sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
+ 
